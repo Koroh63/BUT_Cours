95abd2c32dc908b522b02259b08e0ce6709f776e
Corentin R <rcorentin63@gmail.com>
2024-02-07 08:22:31 +0100

    TP1 Symfony

---
+++ 3A/Symfony/cache/cache/url_matching_routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/quote' => [[['_route' => 'app_quote', '_controller' => 'App\\Controller\\QuoteController::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+    ],
+    [ // $dynamicRoutes
+    ],
+    null, // $checkCondition
+];
